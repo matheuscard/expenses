93456227903a9cae2cd41b18b4683d7a7860c046
phcardoso <profissional.phac@hotmail.com>
2020-12-19 13:43:28 -0300

    first commit

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Título',
+              ),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+              controller: valueController,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FlatButton(
+                  child: Text('Nova Transação'),
+                  textColor: Colors.white,
+                  color: Colors.purple,
+                  onPressed: () {
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, value);
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
